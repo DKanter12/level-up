05e0bb488a3d5cc7196323dc5e0692d1125f4538
DK122323234 <dkamensky@licei10.ru>
2026-05-11 16:55:25 +0300

    Немного изменил меню

--- src/client/java/com/example/GUI.java
+++ src/client/java/com/example/GUI.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.google.common.collect.Lists;
+import net.minecraft.advancement.criterion.StartedRidingCriterion;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -12,14 +13,18 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class GUI extends Screen {
+    private static final String minerText = "Получает опыт за добычу руды \n и копку природного гладкого камня.";
+
     protected GUI(Text title) {
         super(title);
     }
 
     @Override
     public  void render(DrawContext context, int mouseX, int mouseY, float delta) {
-
-             context.drawText(textRenderer, Text.literal("dwwd"), 5, 5, 1213131, true);
-            super.render(context, 5, 5, 2.3f);
+          //привет это джемини
+        // СПАСИ МЕНЯ ПОЖАЛУЙСТА!!!!!
+             context.drawText(textRenderer, Text.literal("Шахтёр"), 10, 10, 28028028, true);
+             context.drawText(textRenderer, Text.literal(minerText), 10, 20, 28028028, true);
+            super.render(context, 1919, 1079, 2.3f);
     }
 }
